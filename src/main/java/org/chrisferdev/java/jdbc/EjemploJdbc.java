4e779f1fc3e5beb45636463efba57187aa036111
Christian <ferchris82@hotmail.com>
2024-01-18 16:56:50 -0500

    Version 1.3

--- src/main/java/org/chrisferdev/java/jdbc/EjemploJdbc.java
+++ src/main/java/org/chrisferdev/java/jdbc/EjemploJdbc.java
@@ -2,6 +2,7 @@ package org.chrisferdev.java.jdbc;
 
 import org.chrisferdev.java.jdbc.modelo.Categoria;
 import org.chrisferdev.java.jdbc.modelo.Producto;
+import org.chrisferdev.java.jdbc.repositorio.CategoriaRepositorioImpl;
 import org.chrisferdev.java.jdbc.repositorio.ProductoRepositorioImpl;
 import org.chrisferdev.java.jdbc.repositorio.Repositorio;
 import org.chrisferdev.java.jdbc.util.ConexionBaseDatos;
@@ -11,26 +12,43 @@ import java.util.Date;
 
 public class EjemploJdbc {
     public static void main(String[] args) throws SQLException {
+        try(Connection conn = ConexionBaseDatos.getConnection()) {
+            if(conn.getAutoCommit()){
+                conn.setAutoCommit(false);
+            }
+            try {
+                Repositorio<Categoria> repositorioCategoria = new CategoriaRepositorioImpl(conn);
+                System.out.println("=========== Insertar nueva categoria ===========");
+                Categoria categoria = new Categoria();
+                categoria.setNombre("Electrohogar");
+                Categoria nuevaCategoria = repositorioCategoria.guardar(categoria);
+                System.out.println("Categoria guardado con éxito: " + nuevaCategoria.getId());
 
-        Repositorio<Producto> repositorio = new ProductoRepositorioImpl();
-        System.out.println("=========== listar ===========");
-        repositorio.listar().forEach(System.out::println);
+                Repositorio<Producto> repositorio = new ProductoRepositorioImpl(conn);
+                System.out.println("=========== listar ===========");
+                repositorio.listar().forEach(System.out::println);
 
-        System.out.println("=========== obtener por id ===========");
-        System.out.println(repositorio.porId(1L));
+                System.out.println("=========== obtener por id ===========");
+                System.out.println(repositorio.porId(1L));
 
-        System.out.println("=========== insertar nuevo producto ===========");
-        Producto producto = new Producto();
-        producto.setNombre("Notebook Notebook Omen HP");
-        producto.setPrecio(2900);
-        producto.setFechaRegistro(new Date());
-        Categoria categoria = new Categoria();
-        categoria.setId(3L);
-        producto.setCategoria(categoria);
-        repositorio.guardar(producto);
-        System.out.println("Producto guardado con éxito");
-        repositorio.listar().forEach(System.out::println);
+                System.out.println("=========== insertar nuevo producto ===========");
+                Producto producto = new Producto();
+                producto.setNombre("Refrigerador Samsung");
+                producto.setPrecio(9900);
+                producto.setFechaRegistro(new Date());
+                producto.setSku("abcdefg123");
 
+                producto.setCategoria(nuevaCategoria);
+                repositorio.guardar(producto);
+                System.out.println("Producto guardado con éxito: " + producto.getId());
+                repositorio.listar().forEach(System.out::println);
+
+                conn.commit();
+            } catch (SQLException e){
+                conn.rollback();
+                e.printStackTrace();
+            }
+        }
     }
 }
 
