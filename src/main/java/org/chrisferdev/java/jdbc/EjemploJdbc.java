01a6650efec81e13e1fdc53d0edfb96a7da45990
Christian <ferchris82@hotmail.com>
2024-01-18 16:09:00 -0500

    Version 1.3

---
+++ src/main/java/org/chrisferdev/java/jdbc/EjemploJdbc.java
@@ -0,0 +1,36 @@
+package org.chrisferdev.java.jdbc;
+
+import org.chrisferdev.java.jdbc.modelo.Categoria;
+import org.chrisferdev.java.jdbc.modelo.Producto;
+import org.chrisferdev.java.jdbc.repositorio.ProductoRepositorioImpl;
+import org.chrisferdev.java.jdbc.repositorio.Repositorio;
+import org.chrisferdev.java.jdbc.util.ConexionBaseDatos;
+
+import java.sql.*;
+import java.util.Date;
+
+public class EjemploJdbc {
+    public static void main(String[] args) {
+
+        Repositorio<Producto> repositorio = new ProductoRepositorioImpl();
+        System.out.println("=========== listar ===========");
+        repositorio.listar().forEach(System.out::println);
+
+        System.out.println("=========== obtener por id ===========");
+        System.out.println(repositorio.porId(1L));
+
+        System.out.println("=========== insertar nuevo producto ===========");
+        Producto producto = new Producto();
+        producto.setNombre("Notebook Notebook Omen HP");
+        producto.setPrecio(2900);
+        producto.setFechaRegistro(new Date());
+        Categoria categoria = new Categoria();
+        categoria.setId(3L);
+        producto.setCategoria(categoria);
+        repositorio.guardar(producto);
+        System.out.println("Producto guardado con éxito");
+        repositorio.listar().forEach(System.out::println);
+
+    }
+}
+
